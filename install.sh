6733cc8e44c383be4917a324dd26a1364cc80c68
Paul Beck <paulbeck@Pauls-MacBook-Pro.local>
2021-09-25 12:25:06 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
-sudo apt update
+sudo apt update -y
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo apt install golang-go
-sudo apt install nodejs
-sudo apt install npm
+    lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo apt install golang-go -y
+sudo apt install nodejs -y
+sudo apt install npm -y
 npm install -g truffle
