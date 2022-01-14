ad2832355148f42dc56ec776fb7bb9c9781a589b
Ubuntu <ubuntu@ip-172-31-2-121.eu-west-1.compute.internal>
2022-01-14 12:17:39 +0000

    push graphs

--- caseStudies/visualizePerformanceTestResults.py
+++ caseStudies/visualizePerformanceTestResults.py
@@ -6,13 +6,16 @@ def main(file_path):
     df = pandas.read_csv(file_path)
     df.boxplot(column="throughput", by="parallel events")
     plt.xticks(rotation=45)
-    plt.title("Boxplot of the throughput by parallel events")
-    plt.subtitle("")
+    plt.title("Boxplot of the throughput by concurrent events")
+    plt.suptitle("")
+    plt.xlabel("Amount of concurrent events")
+    plt.ylabel("Throughput (events/second)")
+    plt.ylim(ymin=0)
     plt.savefig(file_path[:-4]+"Throughput.png")
 
     df.boxplot(column="latency", by="parallel events")
     plt.title("Boxplot of the latency by parallel events")
-    plt.subtitle("")
+    plt.suptitle("")
     plt.savefig(file_path[:-4]+"Latency.png")
 
 if __name__ == "__main__":
