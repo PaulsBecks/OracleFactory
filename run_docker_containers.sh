97a698473b6d1fb07e660241cdaad975511ea7b7
Paul Beck <paulbeck@Pauls-MacBook-Pro.local>
2022-01-23 21:24:24 +0100

    move env infront of name

--- run_docker_containers.sh
+++ run_docker_containers.sh
@@ -9,4 +9,4 @@ docker pull paulsbecks/blf-outbound-oracle
 
 docker network create -d bridge pub-sub-oracle-network
 docker run -p 8080:8080 -d --name pub-sub-oracle --network=pub-sub-oracle-network --add-host=host.docker.internal:host-gateway -v /var/run/docker.sock:/var/run/docker.sock paulsbecks/pub-sub-oracle
-docker run -p 3000:3000 -d --name pub-sub-oracle-frontend --network=pub-sub-oracle-network paulsbecks/pub-sub-oracle-frontend --env REACT_APP_ENV=PROD --env REACT_APP_BASE_URL=$1
+docker run -p 3000:3000 -d --name pub-sub-oracle-frontend --network=pub-sub-oracle-network --env REACT_APP_ENV=PROD --env REACT_APP_BASE_URL=$1 paulsbecks/pub-sub-oracle-frontend 
